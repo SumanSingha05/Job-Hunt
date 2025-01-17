60ad8d5f2ad8059c779117f65f19ce76fbc6db8c
SumanSingha05 <sumansingha8654@gmail.com>
2025-01-17 15:37:55 +0530

    Company Schema and registration done

---
+++ Backend/routes/company.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import { getCompanyById, registerCompany, updateCompany, getCompany } from "../controllers/company.controller.js";
+
+const router = express.Router();
+
+router.route("/register").post(isAuthenticated, registerCompany);
+router.route("/get").get(isAuthenticated, getCompany);
+router.route("/get/:id").get(isAuthenticated, getCompanyById);
+router.route("/profile/update").put(isAuthenticated,updateCompany);
+
+export default router;
