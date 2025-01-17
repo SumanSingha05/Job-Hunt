cec7d0cdfcd6af0be74ca91694fb69f415a6633d
SumanSingha05 <sumansingha8654@gmail.com>
2025-01-17 10:39:52 +0530

    Logout Routing Done

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { register, updateProfile } from "../controllers/user.controller.js";
+import { logout, register, updateProfile } from "../controllers/user.controller.js";
 import { login } from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 
 const router = express.Router();
 
 router.route("/register").post(register);
- router.route("/login").post(login);
+router.route("/login").post(login);
+router.route("/logout").get(logout);
 router.route("/profile/update").post(isAuthenticated,updateProfile);
 
 export default router;
