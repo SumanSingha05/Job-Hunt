ff67eacaca1a7f7f17486704f40d95f8bee4ba80
SumanSingha05 <sumansingha8654@gmail.com>
2025-01-19 12:12:05 +0530

    minor buxs

--- Backend/models/job.model.js
+++ Backend/models/job.model.js
@@ -34,12 +34,18 @@ const jobSchema = new mongoose.Schema({
         ref:'Company',
         required:true
     },
-    created_by:[
+    created_by:
         {
         type:mongoose.Schema.Types.ObjectId,
-        ref:'Application',
-    }
-]
+        ref:'User',
+        required: true
+    },
+    applications: [
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref: 'Application',
+        }
+    ]
 
 },{timestamps:true});
 export const Job = mongoose.model("Job", jobSchema);
