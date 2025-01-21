b7988ff251dd673705c457969f2198fad8dd142b
SumanSingha05 <sumansingha8654@gmail.com>
2025-01-21 10:12:34 +0530

    basic routing done and designing done

--- Frontend/src/components/auth/Signup.jsx
+++ Frontend/src/components/auth/Signup.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Navbar from '../shared/Navbar'
+import { Label } from '../ui/label'
+import { Input } from '../ui/input'
 
 const Signup = () => {
+
   return (
     <div>
-        
+        <Navbar/>
+        <div>
+            <form action=''>
+                <h1 className='font-bold text-xl mb-5'>Sign up</h1>
+                <div className='my-2'>
+                    <Label>Full Name</Label>
+                    <Input
+                    type="text"
+                    placeholder="Suman"
+                    />
+                </div>
+            </form>
+        </div>
     </div>
   )
 }
