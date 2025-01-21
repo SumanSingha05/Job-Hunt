706c8f97a6443a4fcde50493cf3b3972f3d9b431
SumanSingha05 <sumansingha8654@gmail.com>
2025-01-21 10:05:09 +0530

    main routing and home page done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,29 @@
-
-import './App.css'
-import Navbar from './components/ui/shared/Navbar'
+import Signup from './components/auth/Signup'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Navbar from './components/shared/Navbar'
+import Login from './components/auth/Login'
+import Home from './components/Home'
+const appRouter = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/login',
+    element:<Login/>
+  },
+  {
+    path:'/signup',
+    element:<Signup/>
+  },
+  
+])
 
 function App() {
 
   return (
     <>
-      <Navbar/>
+      <RouterProvider router = {appRouter}/>
     </>
   )
 }
