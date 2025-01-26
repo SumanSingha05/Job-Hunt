29baff282cfdd7e84fee8ca2999956724876e2dd
SumanSingha05 <sumansingha8654@gmail.com>
2025-01-26 12:05:56 +0530

    CategoryCarousel initialize

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from './shared/Navbar'
 import HeroSection from './HeroSection'
+import CategoryCarousel from './CategoryCarousel'
 const Home = () => {
   return (
     <div><Navbar/>
     <HeroSection/>
-    {/* <CategoryCarousel/>
-    <LatestJobs/>
+    <CategoryCarousel/>
+    {/* <LatestJobs/>
     <Footer/> */}
     </div>
   )
