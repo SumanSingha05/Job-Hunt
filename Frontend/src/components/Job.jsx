c88647970debf2a885715ec8e57d9820844c13a8
SumanSingha05 <sumansingha8654@gmail.com>
2025-01-27 00:05:31 +0530

    job bookmark feature basics added

--- Frontend/src/components/Job.jsx
+++ Frontend/src/components/Job.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Button } from './ui/button'
+import { Bookmark } from 'lucide-react'
+import { Avatar, AvatarImage } from '@radix-ui/react-avatar'
 
 const Job = () => {
   return (
-    <div>Job</div>
+    <div>
+    <div>
+    <p>2 days ago</p>
+    <Button variant='outline' classname="rounded-full" size='icon'><Bookmark/></Button>
+    </div>
+      
+   <div className='flex items-center gap-2 my-2'>
+    <Button classname="p-6" variant="outline" size="icon">
+      <Avatar>
+        <AvatarImage src="https://www.shutterstock.com/image-vector/circle-line-simple-design-logo-600nw-2174926871.jpg" />
+      </Avatar>
+    </Button>
+    <div>
+      <h1>Company Name</h1>
+      <p>India</p>
+    </div>
+   </div>
+    </div>
   )
 }
 
