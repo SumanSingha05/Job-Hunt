9a0eb8877ce60ea59f6d03ab5b318da258495a4a
SumanSingha05 <sumansingha8654@gmail.com>
2025-01-26 22:04:20 +0530

    front page basic changes

---
+++ Frontend/src/components/LatestJobs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const randomJobs = [1,2,3,4,5,6,7,8];
+export const LatestJobs = () => {
+  return (
+    <div className='max-w-7xl mx-auto my-20'>
+    <h1 className='text-4xl font-bold'><span className='text-[#6A38C2]'>Latest & Top </span>Job Openings</h1>
+        {/* Multiple Job Cards */}
+    </div>
+  )
+}
+
+export default LatestJobs
