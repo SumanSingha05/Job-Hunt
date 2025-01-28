b4b03b0d39dbb30b303e31feb11317c2e0caffbc
SumanSingha05 <sumansingha8654@gmail.com>
2025-01-28 18:39:55 +0530

    resume feature added and in applied jobs the data of applied jobs will be shown

--- Frontend/src/components/AppliedJobTable.jsx
+++ Frontend/src/components/AppliedJobTable.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from './ui/table'
+import { Badge } from './ui/badge'
 
 const AppliedJobTable = () => {
   return (
-    <div>AppliedJobTable</div>
+    <div>
+      <Table>
+        <TableCaption>A list of your applied jobs</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Data</TableHead>
+            <TableHead>Job Role</TableHead>
+            <TableHead>Company</TableHead>
+            <TableHead className='text-right'>Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {
+            [1, 2].map((item, index) => (
+              <TableRow key={index}>
+                <TableCell>28.01.2025</TableCell>
+                <TableCell>Frontend Developer</TableCell>
+                <TableCell>Google</TableCell>
+                <TableCell className='text-right'><Badge>Selected</Badge></TableCell>
+            
+              </TableRow>
+
+            ))
+          }
+
+        </TableBody>
+      </Table>
+    </div>
   )
 }
 
--- Frontend/src/components/Profile.jsx
+++ Frontend/src/components/Profile.jsx
@@ -5,6 +5,7 @@ import { Button } from './ui/button'
 import { Contact, Mail, Pen } from 'lucide-react'
 import { Badge } from './ui/badge'
 import { Label } from './ui/label'
+import AppliedJobTable from './AppliedJobTable'
 
 
 const skills =["html","css","javascript","reactjs"];
@@ -13,7 +14,7 @@ const Profile = () => {
   return (
     <div>
       <Navbar />
-      <div className='mex-w-7xl mx-auto bg-white border border-gray-200 rounded-2xl my-5 p-8'>
+      <div className='mex-w-4xl mx-auto bg-white border border-gray-200 rounded-2xl my-5 p-8'>
         <div className='flex justify-between'>
           <div className='flex items-center gap-4'>
             <Avatar className="h-24 w-24">
@@ -56,11 +57,13 @@ const Profile = () => {
               isResume ? <a target='blank'href='https://youtube.com' className='text-blue-500 w-full hover:underline cursor-pointer'>Suman Singha</a> : <span>NA</span>
             }
       </div>
-      <div className='max-w-4xl mx-auto bg-white rounded-2xl'>
-      <h1>Applied Jobs</h1>
-      {/* application table will be displayed */}
-      </div>
     </div>
+    <div className='max-w-4xl mx-auto bg-white rounded-2xl'>
+      <h1 className='font-bold text-lg my-5'>Applied Jobs</h1>
+      {/* application table will be displayed */}
+       <AppliedJobTable/>
+            
+      </div>
     </div>
   )
 }
