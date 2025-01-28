1719bf5268520b71f680b839ab0430ee351627fb
SumanSingha05 <sumansingha8654@gmail.com>
2025-01-28 11:31:11 +0530

    Browse page initialized

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/shared/Navbar'
 import Login from './components/auth/Login'
 import Home from './components/Home'
 import Jobs from './components/Jobs'
+import Browse from './components/Browse'
 const appRouter = createBrowserRouter([
   {
     path:'/',
@@ -20,6 +21,10 @@ const appRouter = createBrowserRouter([
   {
     path:'/jobs',
     element:<Jobs/>
+  },
+  {
+    path:'/browse',
+    element:<Browse/>
   },
 ])
 
