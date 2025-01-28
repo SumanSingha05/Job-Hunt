6c9dce426bcb464737b7d396c96dea2fc14320ca
SumanSingha05 <sumansingha8654@gmail.com>
2025-01-28 11:55:26 +0530

    after login profile logo adding done

--- Frontend/src/components/shared/Navbar.jsx
+++ Frontend/src/components/shared/Navbar.jsx
@@ -15,9 +15,10 @@ import React from 'react'
 import { User2 } from "lucide-react"
 import { LogOut } from "lucide-react"
 import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
 
 const Navbar = () => {
-    const user = false;
+    const {user} = useSelector(store=>store.auth);
     return (
         <div className='bg-white'>
             <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
