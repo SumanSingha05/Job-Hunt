3affaa9dd4d681b018dab7ea2165256cc3c8aafc
SumanSingha05 <sumansingha8654@gmail.com>
2025-02-03 23:55:30 +0530

    admin setup initialized and route set for admin done

--- Frontend/src/components/shared/Navbar.jsx
+++ Frontend/src/components/shared/Navbar.jsx
@@ -21,24 +21,24 @@ import axios from "axios"
 import { setUser } from "@/redux/authSlice"
 import { USER_API_END_POINT } from "@/utils/constant"
 const Navbar = () => {
-    const {user} = useSelector(store=>store.auth);
-    const dispatch =  useDispatch();
+    const { user } = useSelector(store => store.auth);
+    const dispatch = useDispatch();
     const navigate = useNavigate();
-    
-    const logoutHandler = async () =>{
-        try{
-                const res = await axios.get(`${USER_API_END_POINT}/logout`,{withCredentials:true});
-                if(res.data.success){
-                    dispatch(setUser(null));
-                    navigate("/");
-                    toast.success(res.data.message);
-                }
-        }catch(error){
-        console.log(error);
-        toast.error(error.response.data.message);
+
+    const logoutHandler = async () => {
+        try {
+            const res = await axios.get(`${USER_API_END_POINT}/logout`, { withCredentials: true });
+            if (res.data.success) {
+                dispatch(setUser(null));
+                navigate("/");
+                toast.success(res.data.message);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.response.data.message);
         }
     }
-    
+
     return (
         <div className='bg-white'>
             <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
@@ -47,10 +47,22 @@ const Navbar = () => {
                 </div>
                 <div className='flex items-center gap-12'>
                     <ul className='flex font-medium items-center gap-5'>
-                        
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/Jobs">Jobs</Link></li>
-                        <li><Link to="/Browse">Browse</Link></li>
+                        {
+                            user && user.role == "recruiter" ? (
+                                <>
+                                    <li><Link to="/admin/companies">Companies</Link></li>
+                                    <li><Link to="/admin/jobs">Jobs</Link></li>
+                                </>
+                            ) : (
+                                <>
+                                    <li><Link to="/">Home</Link></li>
+                                    <li><Link to="/Jobs">Jobs</Link></li>
+                                    <li><Link to="/Browse">Browse</Link></li>
+                                </>
+                            )
+                        }
+
+
                     </ul>
                     {
                         !user ? (
@@ -79,16 +91,20 @@ const Navbar = () => {
                                         </div>
                                     </div>
                                     <div className="flex flex-col my-2 text-gray-600">
-                                        <div className='flex w-fit items-center gap-2 cursor-pointer'>
-                                            <User2 />
+                                        {
+                                            user && user.role == 'student' && (
+                                                <div className='flex w-fit items-center gap-2 cursor-pointer'>
+                                                    <User2 />
+                                                    <Button variant="link"><Link to="/profile">View Profile</Link></Button>
+                                                </div>
+                                            )
+                                        }
 
-                                            <Button variant="link"><Link to="/profile">View Profile</Link></Button>
-                                        </div>
                                         <div className="flex w-fit items-center gap-2 cursor-pointer">
                                             <LogOut />
                                             <Button onClick={logoutHandler} variant="link">Logout</Button>
                                         </div>
-                                    </div> 
+                                    </div>
 
                                 </PopoverContent>
                             </Popover>
