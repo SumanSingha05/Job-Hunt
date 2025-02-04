5fe52e51965c8f7f053ac8c39ab7f488429f7735
SumanSingha05 <sumansingha8654@gmail.com>
2025-02-04 18:47:51 +0530

    some basic company registration fields added

--- Frontend/src/components/admin/CompanySetup.jsx
+++ Frontend/src/components/admin/CompanySetup.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from '../shared/Navbar'
+import { Button } from '../ui/button'
+import { ArrowLeft } from 'lucide-react'
+import { Label } from '../ui/label'
+import { Input } from '../ui/input'
+
 
 const CompanySetup = () => {
-  return (
-    <div>companySetup</div>
-  )
+    const [input, setInput] = useState({
+        name: "",
+        description: "",
+        website: "",
+        location: "",
+        file: null
+    });
+
+    const changeEventHandler = (e) => {
+        setInput({ ...input, [e.target.name]: e.target.value })
+    }
+    return (
+        <div>
+            <Navbar />
+            <div className='max-w-xl mx-auto my-10'>
+                <form action=''>
+                    <div className='flex items-center gap-5 p-8'>
+                        <Button variant="outline" className="flex items-center gap-2 text-gray-500 font-semibold">
+                            <ArrowLeft />
+                            <span>Back</span></Button>
+                        <h1 className='font-bold text-xl'>Company Setup</h1>
+                    </div>
+                    <div className='grid grid-cols-2 gap-4'>
+                        <div>
+                            <Label>Company Name</Label>
+                            <Input
+                                type="text"
+                                name="name"
+                                value={input.name}
+                                onChange={changeEventHandler}
+                            />
+                        </div>
+                        <div>
+                            <Label>Description</Label>
+                            <Input
+                                type="text"
+                                name="description"
+                                value={input.description}
+                                onChange={changeEventHandler}
+                            />
+                        </div>
+                        <div>
+                            <Label>Website</Label>
+                            <Input
+                                type="text"
+                                name="website"
+                                value={input.website}
+                                onChange={changeEventHandler}
+                            />
+                        </div>
+                        <div>
+                            <Label>Location</Label>
+                            <Input
+                                type="text"
+                                name="location"
+                                value={input.location}
+                                onChange={changeEventHandler}
+                            />
+                        </div>
+                        <div>
+                            <Label>Logo</Label>
+                            <Input
+                                type="file"
+                                accept="image/*" // here * meaning every type of pic like png, jpeg etc.
+                                onChange={changeFileHandler}
+                            />
+                        </div>
+
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default CompanySetup
