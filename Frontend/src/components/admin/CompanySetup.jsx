b7f4100ee0143364410649be210f2f04c875be4a
SumanSingha05 <sumansingha8654@gmail.com>
2025-02-05 09:19:38 +0530

    after registering a new company the company name will also be shown in the company setup page

--- Frontend/src/components/admin/CompanySetup.jsx
+++ Frontend/src/components/admin/CompanySetup.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../shared/Navbar'
 import { Button } from '../ui/button'
-import { ArrowLeft } from 'lucide-react'
+import { ArrowLeft, Loader2 } from 'lucide-react'
 import { Label } from '../ui/label'
 import { Input } from '../ui/input'
 import { COMPANY_API_END_POINT } from '@/utils/constant'
 import axios from 'axios'
 import { useNavigate, useParams } from 'react-router-dom'
+import { toast } from 'sonner'
+import { useSelector } from 'react-redux'
+import store from '@/redux/store'
 
 const CompanySetup = () => {
     const [input, setInput] = useState({
@@ -15,7 +18,8 @@ const CompanySetup = () => {
         website: "",
         location: "",
         file: null
-    });
+    }); 
+    const {singleCompany} = useSelector(store=>store.company)
     const [loading, setLoading] = useState(false);
     const params = useParams();
     const navigate = useNavigate();
@@ -31,35 +35,49 @@ const CompanySetup = () => {
     const submitHandler = async (e) => {
         e.preventDefault();
         const formData = new FormData();
-        formData.append("name".input.name);
-        formData.append("description".input.description);
-        formData.append("website".input.website);
-        formData.append("location".input.location);
+        formData.append("name",input.name);
+        formData.append("description",input.description);
+        formData.append("website",input.website);
+        formData.append("location",input.location);
         if(input.file){
-            formData.append("file".input.file);
+            formData.append("file",input.file);
         }        
         try{
+            setLoading(true);
             const res = await axios.put(`${COMPANY_API_END_POINT}/update/${params.id}`, formData, {
                 header: {
-                    'Content-Type':'application/json'
+                    'Content-Type':'multipart/form-data'
             },
             withCredentials:true
         });
         if(res.data.success){
             toast.success(res.data.message);
-            
+            navigate("/admin/companies");
         }
         }catch(error){
             console.log(error);
+            toast.error(error.response.data.message);
+        }
+        finally {
+            setLoading(false);
         }
     }
+    useEffect(() => {
+        setInput({
+        name: singleCompany.name || "",
+        description: singleCompany.description || "",
+        website: singleCompany.website || "",
+        location: singleCompany.location || "",
+        file: singleCompany.file || null
+        })
+    },[singleCompany]);
     return (
         <div>
             <Navbar />
             <div className='max-w-xl mx-auto my-10'>
                 <form onSubmit={submitHandler}>
                     <div className='flex items-center gap-5 p-8'>
-                        <Button variant="outline" className="flex items-center gap-2 text-gray-500 font-semibold">
+                        <Button onClick={() => navigate("/admin/companies")}variant="outline" className="flex items-center gap-2 text-gray-500 font-semibold">
                             <ArrowLeft />
                             <span>Back</span></Button>
                         <h1 className='font-bold text-xl'>Company Setup</h1>
@@ -110,7 +128,12 @@ const CompanySetup = () => {
                             />
                         </div>
                     </div>
-                    <Button type="submit" className="w-full mt-8">Update</Button>
+                    
+                    {
+                      loading ? <Button className="w-full my-4">  <Loader2 className='mr-2 h-4 w-4 animate-spin'/> Please Wait
+                      </Button> : <Button type="submit" className="w-full my-4">Update</Button>
+                    }
+                
                 </form>
             </div>
         </div>
