ab011209b9e14f78ecfc0a8fc40a3ea2d9f2d575
SumanSingha05 <sumansingha8654@gmail.com>
2025-02-10 22:56:36 +0530

    minor improvements

--- Frontend/src/components/admin/PostJob.jsx
+++ Frontend/src/components/admin/PostJob.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Navbar from '../shared/Navbar'
+import { Label } from '../ui/label'
+import { Input } from '../ui/input'
 
 const PostJob = () => {
   return (
-    <div>PostJob</div>
+    <div>
+        <Navbar/>
+        <div className='flex items-center justify-center w-screen my-5'>
+            <div>
+                <Label>Title</Label>
+                <Input
+                type="text"
+                name="title"
+                className="focus-visible:ring-offset-0 focus-visible:ring-0"
+            </div>
+        </div>
+    </div>
   )
 }
 
