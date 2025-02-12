a1ba37a2ee9d3d70fb7ce5375a893c451ac3ec75
SumanSingha05 <sumansingha8654@gmail.com>
2025-02-12 11:12:47 +0530

    applications get feature basic things added

--- Frontend/src/components/admin/Applicants.jsx
+++ Frontend/src/components/admin/Applicants.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import Navbar from '../shared/Navbar'
+import ApplicantsTable from './ApplicantsTable'
+import { APPLICATION_API_END_POINT } from '@/utils/constant'
+import { useParams } from 'react-router-dom'
 const Applicants = () => {
+    const params = useParams();
+    useEffect=(() => {
+        const fetchAllApplicants = async () => {
+            try {
+                const res = await axios.get(`${APPLICATION_API_END_POINT}/${params.id}/applicants`, {withCredentials:true});
+            }
+            catch(error){
+                console.log("error");
+            }
+        }
+    },[])
   return (
-    <div>Applicants</div>
+    <div>
+        <Navbar/>
+        <div className='max-w-7xl mx-auto'></div>
+        <h1 className='font-bold text-xl my-5'>Applicants (3) </h1>
+        <ApplicantsTable/>
+    </div>
   )
 }
 
