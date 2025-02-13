4e271875f88e54eac87ffa540fa1e91c663ffa72
SumanSingha05 <sumansingha8654@gmail.com>
2025-02-13 16:46:50 +0530

    applicants routes added

--- Frontend/src/redux/applicationSlice.js
+++ Frontend/src/redux/applicationSlice.js
@@ -6,7 +6,7 @@ const applicationSlice = createSlice ({
 
     },
     reducers: {
-        setApplicants:(state, action) => {
+        setAllApplicants:(state, action) => {
             state.applicants = action.payload;
         }
     }
