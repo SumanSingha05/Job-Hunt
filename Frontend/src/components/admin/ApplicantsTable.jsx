ca59782a4d43f95db396fc7fc40bd65d743cdf33
SumanSingha05 <sumansingha8654@gmail.com>
2025-02-13 16:46:31 +0530

    showing that whom applied in the particular job

--- Frontend/src/components/admin/ApplicantsTable.jsx
+++ Frontend/src/components/admin/ApplicantsTable.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
 import { MoreHorizontal } from 'lucide-react';
+import { useSelector } from 'react-redux';
 
 const shortlistingStatus = ["Accepted", "Rejected"];
 const ApplicantsTable = () => {
+    const { applicants } = useSelector(store => store.application);
     return (
         <div>
             <Table>
@@ -18,36 +20,41 @@ const ApplicantsTable = () => {
                         <TableHead>Date</TableHead>
                         <TableHead className="text-right">Action</TableHead>
                     </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        <tr>
-                            <TableCell>FullName</TableCell>
-                            <TableCell>Email</TableCell>
-                            <TableCell>Contact</TableCell>
-                            <TableCell>Resume</TableCell>
-                            <TableCell>Date</TableCell>
-                            <TableCell className='float-right cursor-pointer'>
-                                <Popover>
-                                    <PopoverTrigger>
-                                        <MoreHorizontal />
-                                    </PopoverTrigger>
-                                    <PopoverContent className='w-32'>
-                                        {
-                                            shortlistingStatus.map((status, index) => {
-                                                return (
-                                                    <div key={index} className='flex w-fit items-center my-2 cursor-pointer '>
-                                                        <span>{status}</span>
-                                                    </div>
-                                                )
-                                            })
-                                        }
-                                    </PopoverContent>
-                                </Popover>
+                </TableHeader>
+                <TableBody>
+                    {
+                        applicants && applicants?.applications?.map((item) => (
+                            <tr key={item._id}>
+                                <TableCell>{item?.applicant?.fullname}</TableCell>
+                                <TableCell>{item?.applicant?.email}</TableCell>
+                                <TableCell>{item?.applicant?.phoneNumber}</TableCell>
+                                <TableCell><a href={item?.applicant?.profile?.resume} target='_blank' rel="noopener noreferrer">{item?.applicant?.profile?.resumeOriginalName}</a></TableCell>
+                                <TableCell>Date</TableCell>
+                                <TableCell className='float-right cursor-pointer'>
+                                    <Popover>
+                                        <PopoverTrigger>
+                                            <MoreHorizontal />
+                                        </PopoverTrigger>
+                                        <PopoverContent className='w-32'>
+                                            {
+                                                shortlistingStatus.map((status, index) => {
+                                                    return (
+                                                        <div key={index} className='flex w-fit items-center my-2 cursor-pointer '>
+                                                            <span>{status}</span>
+                                                        </div>
+                                                    )
+                                                })
+                                            }
+                                        </PopoverContent>
+                                    </Popover>
 
-                            </TableCell>
-                        </tr>
-                    </TableBody>
-                
+                                </TableCell>
+                            </tr>
+                        ))
+                    }
+
+                </TableBody>
+
 
             </Table>
         </div>
