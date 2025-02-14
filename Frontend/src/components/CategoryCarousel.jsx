0fcc04de9cc0cbca869a01436a473f1796a6705d
SumanSingha05 <sumansingha8654@gmail.com>
2025-02-14 16:49:45 +0530

    search job feature properly working

--- Frontend/src/components/CategoryCarousel.jsx
+++ Frontend/src/components/CategoryCarousel.jsx
@@ -4,7 +4,6 @@ import { Button } from './ui/button'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { setSearchedQuery } from '@/redux/jobSlice'
-
 const category = [
   "Frontend Developer",
   "Backend Developer",
