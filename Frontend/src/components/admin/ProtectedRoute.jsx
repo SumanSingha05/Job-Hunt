efad78908981f21672dcacb1d99722d9670cdda2
SumanSingha05 <sumansingha8654@gmail.com>
2025-02-16 10:17:07 +0530

    protected routes updated

--- Frontend/src/components/admin/ProtectedRoute.jsx
+++ Frontend/src/components/admin/ProtectedRoute.jsx
@@ -2,6 +2,8 @@ const { useEffect } = require("react");
 const { useSelector } = require("react-redux");
 const { useNavigate } = require("react-router-dom");
 
+
+
 const ProtectedRoute = ({children}) => {
     const{user} = useSelector(store=>store.auth);
 
