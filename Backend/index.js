9afa1bb06fcb593fab92808371550f768aef12ad
SumanSingha05 <sumansingha8654@gmail.com>
2025-04-25 01:22:31 +0530

    some minor changes in backend code

--- Backend/index.js
+++ Backend/index.js
@@ -7,16 +7,20 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
+
 dotenv.config({});
 const app = express();
 
+const _dirname = path.resolve();
+
 //Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin: 'http://localhost:5173',
     credentials: true
 }
 
@@ -30,7 +34,12 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-app.listen(PORT, () =>{
+app.use(express.static(path.join(_dirname, "/frontend/dist")))
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(_dirname,))
+})
+
+app.listen(PORT, () => {
     connectDB();
     console.log(`Server running at port ${PORT}`);
 
