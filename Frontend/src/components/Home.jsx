a8c5b50b37be881f65cb8d896b63f6ff48d349c7
SumanSingha05 <sumansingha8654@gmail.com>
2025-04-25 12:16:40 +0530

    Add the chat bot to the home page

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -2,27 +2,29 @@ import React, { useEffect } from 'react'
 import Navbar from './shared/Navbar'
 import HeroSection from './HeroSection'
 import CategoryCarousel from './CategoryCarousel'
-import LatestJobs  from './LatestJobs'
+import LatestJobs from './LatestJobs'
 import Footer from './shared/Footer'
 import userGetAllJobs from '@/hooks/useGetAllJobs'
 import { useSelector } from 'react-redux'
 import store from '@/redux/store'
 import { useNavigate } from 'react-router-dom'
+import ChatBot from '@/Chat/ChatBot'
 const Home = () => {
   userGetAllJobs();
-  const {user} = useSelector(store=>store.auth);
+  const { user } = useSelector(store => store.auth);
   const navigate = useNavigate();
   useEffect(() => {
-    if(user?.role == 'recruiter'){
+    if (user?.role == 'recruiter') {
       navigate("/admin/companies")
     }
-  },[]);
+  }, []);
   return (
-    <div><Navbar/>
-    <HeroSection/>
-    <CategoryCarousel/>
-    <LatestJobs/>
-    <Footer/> 
+    <div><Navbar />
+      <HeroSection />
+      <CategoryCarousel />
+      <LatestJobs />
+      <ChatBot />
+      <Footer />
     </div>
   )
 }
